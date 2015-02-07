0f04e5f6a938e07e3a86e733b2dcae48e39240b0
JohnWest <johnwest@umich.edu>
2015-02-07 16:24:21 -0500

    Initial Commit

---
+++ EECS 281 Project 2/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  EECS 281 Project 2
+//
+//  Created by John West on 2/7/15.
+//  Copyright (c) 2015 John West. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+	// insert code here...
+	std::cout << "Hello, World!\n";
+    return 0;
+}
